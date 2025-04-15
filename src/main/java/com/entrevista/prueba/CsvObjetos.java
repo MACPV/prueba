03a358fe7022ffc48aa3f12a6a595363ae791a57
MACPV <aldo.pintado.98@gmail.com>
2025-04-15 02:56:22 -0500

    creacion de CSVObjetos
    esta clase crea un objeto con cada línea del archivo csv

---
+++ src/main/java/com/entrevista/prueba/CsvObjetos.java
@@ -0,0 +1,23 @@
+package com.entrevista.prueba;
+
+import com.opencsv.bean.CsvBindByName;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class CsvObjetos {
+    @CsvBindByName
+    private int id;
+
+    @CsvBindByName
+    private String tipo;
+
+    @CsvBindByName
+    private double valor;
+
+
+}
